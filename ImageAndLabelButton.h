b32eb3f4b90d45600543dfebbc7d62ff08c78c5e
Brian Hammond <brian@fictorial.com>
2012-05-09 13:29:46 -0400

    Add support for on/off images in ImageAndLabelButton

--- ImageAndLabelButton.h
+++ ImageAndLabelButton.h
@@ -14,44 +14,59 @@
 	CCLabelTTF* label;
 }
 
-@property(nonatomic,readonly,retain) CCLabelTTF* label;
+@property (nonatomic,readonly,retain) CCLabelTTF* label;
+@property (nonatomic, assign) CGPoint labelOffsetWhenDown;
 
 /** Create a new button.
- * @param filename the filename of the image to use as a touchable portion.
- * @param label the label to display to the right of the image.
+ * @param filename the filename of the image to use as a touchable portion when pressed and not pressed.
+ * @param label the label to display atop the image.
  * @param target the target to notify when the button is pressed.
  * @param selector the selector to call when the button is pressed.
  * @return a new button.
  */
-+ (id) buttonWithImageFile:(NSString*) filename label:(CCLabelTTF*) label target:(id) target selector:(SEL) selector;
++ (id)buttonWithImageFile:(NSString *)filename 
+                    label:(CCLabelTTF *)label
+                   target:(id)target
+                 selector:(SEL)selector;
 
 /** Create a new button.
- * @param sprite the sprite to use as a touchable portion.
- * @param label the label to display to the right of the image.
+ * @param normalSprite the sprite to use as a touchable portion when not pressed.
+ * @param selectedSprite the sprite to use as a touchable portion when pressed.
+ * @param label the label to display atop the image.
  * @param target the target to notify when the button is pressed.
  * @param selector the selector to call when the button is pressed.
  * @return a new button.
  */
-+ (id) buttonWithSprite:(CCSprite *) sprite label:(CCLabelTTF*) label target:(id) target selector:(SEL) selector;
++ (id)buttonWithSprite:(CCSprite *)normalSprite
+        selectedSprite:(CCSprite *)selectedSprite
+                 label:(CCLabelTTF*)label 
+                target:(id)target
+              selector:(SEL)selector;
 
 /** Initialize a button.
- * @param filename the filename of the image to use as a touchable portion.
- * @param label the label to display to the right of the image.
+ * @param filename the filename of the image to use as a touchable portion when pressed and not pressed.
+ * @param label the label to display atop the image.
  * @param target the target to notify when the button is pressed.
  * @param selector the selector to call when the button is pressed.
  * @return the initialized button.
  */
-- (id) initWithImageFile:(NSString*) filename label:(CCLabelTTF*) label target:(id) target selector:(SEL) selector;
+- (id)initWithImageFile:(NSString *)filename 
+                  label:(CCLabelTTF *)label
+                 target:(id)target
+               selector:(SEL)selector;
 
 /** Initialize a button.
- * @param sprite the sprite to use as a touchable portion.
- * @param label the label to display to the right of the image.
+ * @param normalSprite the sprite to use as a touchable portion when not pressed.
+ * @param selectedSprite the sprite to use as a touchable portion when pressed.
+ * @param label the label to display atop the image.
  * @param target the target to notify when the button is pressed.
  * @param selector the selector to call when the button is pressed.
  * @return the initialized button.
  */
-- (id) initWithSprite:(CCSprite *) sprite label:(CCLabelTTF*) label target:(id) target selector:(SEL) selector;
-
-
+- (id)initWithSprite:(CCSprite *)normalSprite 
+      selectedSprite:(CCSprite *)selectedSprite
+               label:(CCLabelTTF *)labelIn
+              target:(id)targetIn
+            selector:(SEL)selectorIn;
 
 @end
--- ImageAndLabelButton.m
+++ ImageAndLabelButton.m
@@ -8,52 +8,117 @@
 #import "ImageAndLabelButton.h"
 #import "CCNode+ContentSize.h"
 
+enum {
+    kNormalSpriteTag = 787878,
+    kSelectedSpriteTag,
+    kLabelTag
+};
 
 #pragma mark ImageAndLabelButton
 
 @implementation ImageAndLabelButton
 
+@synthesize labelOffsetWhenDown;
+
 #pragma mark Object Management
 
-+ (id) buttonWithImageFile:(NSString*) filename label:(CCLabelTTF*) label target:(id) target selector:(SEL) selector
++ (id)buttonWithImageFile:(NSString *)filename
+                    label:(CCLabelTTF *)label
+                   target:(id)target
+                 selector:(SEL)selector
 {
-	return [[[self alloc] initWithImageFile:filename label:label target:target selector:selector] autorelease];
+	return [[[self alloc] initWithImageFile:filename
+                                      label:label
+                                     target:target
+                                   selector:selector] autorelease];
 }
 
-+ (id) buttonWithSprite:(CCSprite *) sprite label:(CCLabelTTF*) label target:(id) target selector:(SEL) selector
++ (id)buttonWithSprite:(CCSprite *)normalSprite
+        selectedSprite:(CCSprite *)selectedSprite
+                 label:(CCLabelTTF*)label 
+                target:(id)target
+              selector:(SEL)selector
 {
-    return [[[self alloc] initWithSprite:sprite label:label target:target selector:selector] autorelease];
+    return [[[self alloc] initWithSprite:normalSprite 
+                          selectedSprite:selectedSprite
+                                   label:label
+                                  target:target
+                                selector:selector] autorelease];
 }
 
-- (id) initWithImageFile:(NSString*) filename label:(CCLabelTTF*) labelIn target:(id) targetIn selector:(SEL) selectorIn
+- (id)initWithImageFile:(NSString *)filename
+                  label:(CCLabelTTF *)labelIn 
+                 target:(id)targetIn
+               selector:(SEL)selectorIn
 {
 	CCSprite* sprite = [CCSprite spriteWithFile:filename];
-    return [self initWithSprite:sprite label:labelIn target:targetIn selector:selectorIn];
+    return [self initWithSprite:sprite selectedSprite:sprite label:labelIn target:targetIn selector:selectorIn];
 }
 
-- (id) initWithSprite:(CCSprite *) sprite label:(CCLabelTTF*) labelIn target:(id) targetIn selector:(SEL) selectorIn
+- (id)initWithSprite:(CCSprite *)normalSprite 
+      selectedSprite:(CCSprite *)selectedSprite
+               label:(CCLabelTTF *)labelIn
+              target:(id)targetIn
+            selector:(SEL)selectorIn
 {
     label = labelIn;
 	
-	float maxHeight = sprite.contentSize.height;
-	if(label.contentSize.height > maxHeight)
+	float maxHeight = normalSprite.contentSize.height;
+	if (label.contentSize.height > maxHeight)
 	{
 		maxHeight = label.contentSize.height;
 	}
 	
 	float halfHeight = maxHeight * 0.5f;
 	
-	sprite.anchorPoint = ccp(0.5f, 0.5f);
-	sprite.position = ccp(sprite.contentSize.width*0.5f, halfHeight);
-	label.anchorPoint = ccp(0.5f, 0.5f);
-	label.position = ccp(sprite.position.x + sprite.contentSize.width*0.5f + label.contentSize.width*0.5f, halfHeight);
+	normalSprite.anchorPoint = selectedSprite.anchorPoint = ccp(0.5f, 0.5f);
+	normalSprite.position = selectedSprite.position = ccp(normalSprite.contentSize.width*0.5f, halfHeight);
+    
+    normalSprite.tag = kNormalSpriteTag;
+    selectedSprite.tag = kSelectedSpriteTag;
+    
+	label.position = normalSprite.position;
+    label.tag = kLabelTag;
 	
 	CCNode* node = [CCNode node];
-	[node addChild:sprite];
+	[node addChild:selectedSprite];
+	[node addChild:normalSprite];
 	[node addChild:label];
 	[node setContentSizeFromChildren];
 	
-	return [super initWithTouchablePortion:node target:targetIn selector:selectorIn];
+    return [super initWithTouchablePortion:node target:targetIn selector:selectorIn];
+}
+
+- (void)onButtonDown 
+{
+    [super onButtonDown];
+
+    CCNode *normalSprite = [self.touchablePortion getChildByTag:kNormalSpriteTag];
+    [self.touchablePortion reorderChild:normalSprite z:0];
+
+    CCNode *selectedSprite = [self.touchablePortion getChildByTag:kSelectedSpriteTag];
+    [self.touchablePortion reorderChild:selectedSprite z:1];
+
+    CCNode *theLabel = [self.touchablePortion getChildByTag:kLabelTag];
+    [self.touchablePortion reorderChild:theLabel z:2];
+    
+    theLabel.position = ccpAdd(theLabel.position, self.labelOffsetWhenDown);
+}
+
+- (void)onButtonUp
+{
+    [super onButtonUp];
+    
+    CCNode *normalSprite = [self.touchablePortion getChildByTag:kNormalSpriteTag];
+    [self.touchablePortion reorderChild:normalSprite z:1];
+    
+    CCNode *selectedSprite = [self.touchablePortion getChildByTag:kSelectedSpriteTag];
+    [self.touchablePortion reorderChild:selectedSprite z:0];
+    
+    CCNode *theLabel = [self.touchablePortion getChildByTag:kLabelTag];
+    [self.touchablePortion reorderChild:theLabel z:2];
+    
+    theLabel.position = ccpSub(theLabel.position, self.labelOffsetWhenDown);
 }
 
 @synthesize label;
